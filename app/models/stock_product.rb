162d7925a73293319f96d43b8f42e7d3453bc6e9
Sy Rashid <sy.rashid.11@gmail.com>
2019-08-20 18:41:08 +0200

    FEAT: Added seeds to seed files

--- app/models/stock_product.rb
+++ app/models/stock_product.rb
@@ -1,7 +1,7 @@
 class StockProduct < ApplicationRecord
   belongs_to :condition
-  belongs_to :cart
+  belongs_to :cart, optional: true
   belongs_to :product
-  belongs_to :box
+  belongs_to :box, optional: true
   has_many :photos, dependent: :destroy
 end
