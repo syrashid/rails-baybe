9dbb9ebb00284e7ef2f36cbab9abf529e2d0d79b
Sy Rashid <sy.rashid.11@gmail.com>
2019-08-20 12:27:46 +0200

    STRUCT: Added a cart_option model and relation, acts as a join table

---
+++ app/models/cart_option.rb
@@ -0,0 +1,4 @@
+class CartOption < ApplicationRecord
+  belongs_to :option
+  belongs_to :cart
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_101958) do
+ActiveRecord::Schema.define(version: 2019_08_20_102600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_options", force: :cascade do |t|
+    t.string "content"
+    t.bigint "option_id"
+    t.bigint "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_cart_options_on_cart_id"
+    t.index ["option_id"], name: "index_cart_options_on_option_id"
+  end
 
   create_table "carts", force: :cascade do |t|
     t.decimal "total_price"
@@ -47,5 +57,7 @@ ActiveRecord::Schema.define(version: 2019_08_20_101958) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cart_options", "carts"
+  add_foreign_key "cart_options", "options"
   add_foreign_key "carts", "users"
 end
