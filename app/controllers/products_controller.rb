c51d2ed6dc160fbb5b6f24968d7d50860c24dad3
Cihad Saglam <saglam.chd@gmail.com>
2019-08-21 15:40:33 +0200

    product index page and product page particaly working but needd to work some design part

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,5 +2,10 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     @categories = Category.all
+    @stoc
+  end
+
+  def show
+    @product = Product.find(params[:id])
   end
 end
