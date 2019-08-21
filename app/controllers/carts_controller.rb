d82d9356a52de37de43988355aaeef7f6565f69c
Kaveh Jalilian <kavehjalilian@hotmail.com>
2019-08-21 13:46:48 +0200

    update user cart route + view + controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,23 @@
 class CartsController < ApplicationController
+  def index
+    @cart = @current_cart
+  end
 
+  def show
+    @cart = @current_cart
+  end
+
+  def total_price
+    self.quantity * self.product.price
+  end
+
+  def destroy
+    @cart = @current_cart
+    @cart.destroy
+    session[:cart_id] = nil
+    redirect_to root_path
+  end
 end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'carts/show'
   get 'categories/index'
   devise_for :users
   root to: 'pages#home'
+  get 'carts/:id' => "carts#show", as: "cart"
+  delete 'carts/:id' => "carts#destroy"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products, only: [ :index ]
+  resources :carts, only: [ :index ]
 end
