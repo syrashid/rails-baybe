5cb3f3f9a6cc71755ebcc49e3df0ca25e666f36b
Cihad Saglam <saglam.chd@gmail.com>
2019-08-22 12:56:43 +0200

    show method in boxesController and routes and show page working on it

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -2,4 +2,8 @@ class BoxesController < ApplicationController
   def index
     @boxes = Box.where("user_id=?", current_user.id)
   end
+
+  def show
+    @stockproducts = Box.find(params[:id]).stock_products
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :categories, only: [ :index, :show]
 
   resources :carts, only: [ :index ]
-  resources :boxes, only: [ :index ]
+  resources :boxes, only: [ :index, :show ]
 end
