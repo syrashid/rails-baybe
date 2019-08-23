33762e902ef9a786814b6c84ad890c74a0680d31
Sy Rashid <sy.rashid.11@gmail.com>
2019-08-23 11:32:14 +0200

    FEAT: Added a people also bought section to cart show page

--- app/controllers/sell_products_controller.rb
+++ app/controllers/sell_products_controller.rb
@@ -1,4 +1,14 @@
 class SellProductsController < ApplicationController
+  def index
+    @categories = Category.all
+    # Do I need to do some protection
+    if params[:query].present?
+      @products = Product.search_by_name_and_description(params[:query])
+    else
+      @products = Product.all
+    end
+  end
+
   def show
     @product = Product.find(params[:id])
     @rel_products = Product.all.sample(4)
