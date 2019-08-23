0c232a6c8436c96570a0acc576a87bffdf74fcf0
Nico Proto <nicolasproto@gmail.com>
2019-08-23 13:03:54 +0200

    FIX: Changed filter to links to all products and added style

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
   def index
     @categories = Category.all
+    @age_groups = ['0 - 3', '3 - 6', '6 - 9', '9 - 12']
+    @condition_groups = ['Like New', 'Very Good', 'Good', 'Acceptable']
     # Do I need to do some protection
     if params[:query].present?
       @products = Product.search_by_name_and_description(params[:query])
