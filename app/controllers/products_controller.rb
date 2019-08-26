3c299651c8077040e6e43aa7742d5edbd1a38e21
Cihad Saglam <saglam.chd@gmail.com>
2019-08-26 15:11:11 +0200

    started payment and stripe

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,7 +17,7 @@ class ProductsController < ApplicationController
     @rel_products = Product.all.sample(4)
     @age_groups = ['0 - 3', '3 - 6', '6 - 9', '9 - 12']
     @condition_groups = ['Like New', 'Very Good', 'Good', 'Acceptable']
-    @color_groups = ['Red', 'Black', 'Blue', 'Braun']
+    @color_groups = ['Red', 'Black', 'Blue', 'Brown']
     @user = current_user
     @carts = Cart.where("user_id=?", @user.id)
     @currentcart = @carts.find_by(paid: "Pending")
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,4 +3,6 @@ class Cart < ApplicationRecord
   has_many :stock_products
   has_many :options, through: :cart_options
   validates :paid, presence: true, inclusion: { in: %w[Pending Paid Denied] }
+
+  # monetize :total_price
 end
