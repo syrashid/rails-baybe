f48fb1ebdd42ec7b737fa78c44ce8cc38610a6c4
Sy Rashid <sy.rashid.11@gmail.com>
2019-08-27 15:35:17 +0200

    Revert "FEAT: Changed button functionality when no items in cart"

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -7,7 +7,7 @@ class CartsController < ApplicationController
   end
 
   def show
-    @cart = Cart.find(params[:id])
+    @cart = Cart .find(params[:id])
     @stockproducts = @cart.stock_products
     @rel_products = Product.all.sample(4)
   end
