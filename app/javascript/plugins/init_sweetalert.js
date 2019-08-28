c0b7c22cd9a2e466ca610518da53ba36c86549e6
Cihad Saglam <saglam.chd@gmail.com>
2019-08-28 17:46:21 +0200

    edit button work sweetalert but this time used ajax

--- app/javascript/plugins/init_sweetalert.js
+++ app/javascript/plugins/init_sweetalert.js
@@ -1,59 +1,70 @@
 import swal from 'sweetalert';
 
-const initSweetalert = (selector, options = {}) => {
-  const swalButton = document.querySelector(selector);
-  if (swalButton) { // protect other pages
-    swalButton.addEventListener('click', () => {
-      swal(options);
-    });
-  }
-};
 
-const initSweetalertCallback = (selector, options = {}, callback = () => {}) => {
-  const swalButton = document.querySelector(selector);
-  if (swalButton) { // protect other pages
-    swalButton.addEventListener('click', () => {
-      swal(options).then(callback); // <-- add the `.then(callback)`
-    });
-  }
-};
+global.swal = swal
+// const deleteButtons = document.querySelectorAll('.sweet-alert-demo');
 
-const currentCart = document.querySelector('.currentcart')
-if (currentCart) {
-  swal({
-    title: "Your Product",
-    text: "Selected produckt added successfully!",
-    icon: "success",
-    timer: 4000
-  })
-}
+// const initSweetalert = () => {
+//   deleteButtons.forEach(button => {
+//     button.addEventListener('click', (e) => {
+//       const btn = e.currentTarget
+//       swal({
+//         title: btn.dataset.swalTitle,
+//         text: btn.dataset.swalText,
+//         icon: btn.dataset.swalIcon
+//       })
+//     });
+//   });
+// };
+
+// initSweetalert();
 
 
-const order = document.querySelector('.order-title')
-if (order) {
-  swal({
-    title: "Your Product",
-    text: "You order Paid, as soon as will be traking in your adress!",
-    icon: "success",
-    timer: 5000
-  })
-}
+// const initSweetalertCallback = (selector, options = {}, callback = () => {}) => {
+//   deleteButtons.forEach(button => {
+//     if (button) { // protect other pages
+//       button.addEventListener('click', () => {
+//         swal(options).then(callback); // <-- add the `.then(callback)`
+//       });
+//     }
+//   })
+// };
+
+// const currentCart = document.querySelector('.currentcart')
+// if (currentCart) {
+//   swal({
+//     title: "Your Product",
+//     text: "Selected produckt added successfully!",
+//     icon: "success",
+//     timer: 4000
+//   })
+// }
 
 
+// const order = document.querySelector('.order-title')
+// if (order) {
+//   swal({
+//     title: "Your Product",
+//     text: "You order Paid, as soon as will be traking in your adress!",
+//     icon: "success",
+//     timer: 5000
+//   })
+// }
 
-initSweetalertCallback('#sweet-alert-demo', {
-  title: "Are you sure?",
-  text: "Are you sure the delete product?",
-  icon: "warning",
-  buttons: true,
-  dangerMode: true,
-}, (value) => {
-  if (value) {
-    const link = document.querySelector('#delete-link');
-    link.click();
-  }
-});
 
-export { initSweetalert };
-export { initSweetalertCallback };
+// initSweetalertCallback(deleteButtons, {
+//   title: "Are you sure?",
+//   text: "Are you sure the delete product?",
+//   icon: "warning",
+//   buttons: true,
+//   dangerMode: true,
+// }, (value) => {
+//   if (value) {
+//     const link = document.querySelector('.delete-link');
+//     link.click();
+//   }
+// });
 
+// export { initSweetalert };
+
+// export { initSweetalertCallback };
