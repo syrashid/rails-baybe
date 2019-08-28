db3955341af9684226d4073e4e81bbfbc7a7142f
Sy Rashid <sy.rashid.11@gmail.com>
2019-08-28 10:59:38 +0200

    FIX: Trying to fix cart and fucking failing

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -7,8 +7,7 @@ class CartsController < ApplicationController
   end
 
   def show
-    @cart = Cart.find(params[:id])
-    @stockproducts = @cart.stock_products
+    @stockproducts = @currentcart.stock_products
     @rel_products = Product.all.sample(4)
   end
 
