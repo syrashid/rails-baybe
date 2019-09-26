56eee8385c7071c2d9131db767483e45f012c38e
Sy Rashid <sy.rashid.11@gmail.com>
2019-09-26 07:01:32 -0400

    FIX: Commented out sweet alert ability to trigger

--- app/javascript/plugins/init_sweetalert.js
+++ app/javascript/plugins/init_sweetalert.js
@@ -1,33 +1,33 @@
 import swal from 'sweetalert';
 import Swal from 'sweetalert2'
 
-global.swal = swal
+// global.swal = swal
 
 
 global.SwalTwo = Swal
 
-const currentCart = document.querySelector('.currentcart')
-if (currentCart) {
-  currentCart.querySelectorAll("[data-swal='true']").forEach(btn => {
-    btn.addEventListener("click", (e) => {
-      const clicked = e.currentTarget;
-      if (clicked.dataset.confirmed == "false") {
-        e.stopImmediatePropagation()
-        e.preventDefault()
-        swal({
-          title: "Are you sure?",
-          text: clicked.dataset.text,
-          icon: "warning",
-          buttons: true,
-          dangerMode: true,
-        })
-        .then((willDelete) => {
-          if (willDelete) {
-            clicked.dataset.confirmed = "true"
-            clicked.click()
-          }
-        })
-      }
-    })
-  })
-}
+// const currentCart = document.querySelector('.currentcart')
+// if (currentCart) {
+//   currentCart.querySelectorAll("[data-swal='true']").forEach(btn => {
+//     btn.addEventListener("click", (e) => {
+//       const clicked = e.currentTarget;
+//       if (clicked.dataset.confirmed == "false") {
+//         e.stopImmediatePropagation()
+//         e.preventDefault()
+//         swal({
+//           title: "Are you sure?",
+//           text: clicked.dataset.text,
+//           icon: "warning",
+//           buttons: true,
+//           dangerMode: true,
+//         })
+//         .then((willDelete) => {
+//           if (willDelete) {
+//             clicked.dataset.confirmed = "true"
+//             clicked.click()
+//           }
+//         })
+//       }
+//     })
+//   })
+// }
