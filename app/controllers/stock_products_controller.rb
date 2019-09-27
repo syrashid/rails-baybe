9c01e7f6487760d63a4aee136bf08ac006509f96
Nico Proto <nicolasproto@gmail.com>
2019-09-27 22:08:17 +0200

    FIX: Refactored destroy stock product method

--- app/controllers/stock_products_controller.rb
+++ app/controllers/stock_products_controller.rb
@@ -1,16 +1,21 @@
 class StockProductsController < ApplicationController
   def destroy
     @stockproduct = StockProduct.find(params[:id])
-    parent = params[:parent]
-    if parent == "box"
-      @stockproduct.update_attributes(box_id: nil)
-    elsif parent == "cart"
-      @stockproduct.update_attributes(cart_id: nil)
-    end
+    @parent = params[:parent]
+
+    update_parent
 
     respond_to do |format|
       format.js
       format.html { redirect_back(fallback_location: root_path) }
+    end
+  end
+
+  def update_parent
+    if @parent == "box"
+      @stockproduct.update_attributes(box_id: nil)
+    elsif @parent == "cart"
+      @stockproduct.update_attributes(cart_id: nil)
     end
   end
 end
