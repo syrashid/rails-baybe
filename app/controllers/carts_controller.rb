1d377df18fb5d8f3a8e4e251a79a5de853d07e32
Nico Proto <nicolasproto@gmail.com>
2019-09-27 16:21:02 +0200

    FIX: Refactored @rel_products @cart_savings @stockproducts and commented out the gift option

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,50 +1,37 @@
 class CartsController < ApplicationController
-  before_action :find_info, only: [ :index, :current, :show, :sum_total]
+  before_action :find_info, only: [:index, :current, :show, :sum_total]
+  before_action :find_stock, only: [:show, :current, :sum_total]
   after_action :sum_total, only: :show
   before_action :sum_total, only: [:current, :show]
+  before_action :rel_and_savings, only: [:show, :current]
 
   def index
     @activecarts = @carts.active
   end
 
   def show
-    @stockproducts = @currentcart.stock_products
-    @rel_products = Product.all.sample(4)
-
-    @cart_savings = savings
-
   end
 
   def current
-    @rel_products = Product.all.sample(4)
-    @stockproducts = @currentcart.stock_products
-    @cart_savings = savings
   end
 
   def confirm
     @cart = current_user.current_cart
-    user = Stripe::Customer.create(
-      source: params[:stripeToken],
-      email:  params[:stripeEmail]
-    )
+    user = Stripe::Customer.create(source: params[:stripeToken], email: params[:stripeEmail])
 
-    charge = Stripe::Charge.create(
-      customer:     user.id, # You should store this customer id and re-use it.
-      amount:       @cart.total_price_cents,
-      description:  "Payment for cart #{@cart.id}",
-      currency:     @cart.total_price.currency
-    )
+    charge = Stripe::Charge.create(customer: user.id, amount: @cart.total_price_cents,
+      description: "Payment for cart #{@cart.id}", currency: @cart.total_price.currency)
 
     # CART OPTIONS PART
-    if params[:gift] == "on"
-      @gift_option = Option.find_by("name=?", "gift")
+    # if params[:gift] == "on"
+    #   @gift_option = Option.find_by("name=?", "gift")
 
-      @cart_gift_option = CartOption.new
-      @cart_gift_option.cart = @cart
-      @cart_gift_option.option = @gift_option
+    #   @cart_gift_option = CartOption.new
+    #   @cart_gift_option.cart = @cart
+    #   @cart_gift_option.option = @gift_option
 
-      @cart_gift_option.save!
-    end
+    #   @cart_gift_option.save!
+    # end
 
     @address_option = Option.find_by("name=?", "address")
 
@@ -55,7 +42,6 @@ class CartsController < ApplicationController
     params[:delivery_place] == "home" ? @address = current_user.address : @address = "#{params[:address]}, #{params[:address2]}, #{params[:city]}, #{params[:country]}, #{params[:zip]}"
 
     @cart_address_option.content = @address
-
     @cart_address_option.save!
 
     # -----------------
@@ -69,9 +55,12 @@ class CartsController < ApplicationController
 
   private
 
+  def find_stock
+    @stockproducts = @currentcart.stock_products
+  end
+
   def sum_total
-    stockproducts = @currentcart.stock_products
-    @currentcart.total_price = stockproducts.sum do |stockproduct|
+    @currentcart.total_price = @stockproducts.sum do |stockproduct|
       stockproduct.product.price * stockproduct.condition.buy_ratio
     end
     @currentcart.save
@@ -89,5 +78,10 @@ class CartsController < ApplicationController
       saving += ((1 - stockprod.condition.buy_ratio) * stockprod.product.price)
     end
     saving
+  end
+
+  def rel_and_savings
+    @rel_products = Product.all.sample(4)
+    @cart_savings = savings
   end
 end
