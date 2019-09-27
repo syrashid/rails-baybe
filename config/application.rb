e83ed2a06763b2f21bd5f8b76d1bc0e65a1e4253
Sy Rashid <sy.rashid.11@gmail.com>
2019-09-27 16:41:35 -0700

    FIX: Still fixing the precompile thing"

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-# tell Rails not to bootstrap your application during asset compilation
-config.assets.initialize_on_precompile = false
+
+
 
 module RailsBaybe
   class Application < Rails::Application
@@ -23,5 +23,8 @@ module RailsBaybe
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # tell Rails not to bootstrap your application during asset compilation
+    config.assets.initialize_on_precompile = false
   end
 end
