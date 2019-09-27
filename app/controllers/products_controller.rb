1f739de9289f327b3d4de3287f4a9534cd1fa47b
Nico Proto <nicolasproto@gmail.com>
2019-09-27 18:38:42 +0200

    FIX: Refactored create product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -60,11 +60,7 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    if @product.save
-      redirect_to root_path
-    else
-      render :new
-    end
+    @product.save ? (redirect_to root_path) : (render :new)
   end
 
   def edit
