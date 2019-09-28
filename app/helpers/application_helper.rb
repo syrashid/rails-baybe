2d14c34728b99fb42f80b45b7d078de8b856ada4
Nico Proto <nicolasproto@gmail.com>
2019-09-28 11:32:54 +0200

    FIX: Removed the selling/buyer theme, now it's all render in different pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def theme
-    if params[:controller] == "sell_products" || params[:controller] == "boxes"
-      return "selling"
-    else
-      return ""
-    end
-  end
 end
