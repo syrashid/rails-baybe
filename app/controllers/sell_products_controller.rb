a38655f02dd26a7a6133250f2a437174a320d663
Sy Rashid <sy.rashid.11@gmail.com>
2019-09-28 23:56:12 -0700

    FIX: updated sell product as well for the brown thing

--- app/controllers/sell_products_controller.rb
+++ app/controllers/sell_products_controller.rb
@@ -58,7 +58,7 @@ class SellProductsController < ApplicationController
     @rel_products = Product.all.sample(4)
     @age_groups = ['0 - 3', '3 - 6', '6 - 9', '9 - 12']
     @condition_groups = ['Like New', 'Very Good', 'Good', 'Acceptable']
-    @color_groups = ['Red', 'Black', 'Blue', 'Braun']
+    @color_groups = ['Red', 'Black', 'Blue', 'Brown']
   end
 
   def find_prod
