5a7ffbae8a7da477b16c1347c5296b6f08f2813f
Nico Proto <nicolasproto@gmail.com>
2019-09-29 22:25:17 +0200

    FIX: Small refactor

--- app/javascript/components/price_calc.js
+++ app/javascript/components/price_calc.js
@@ -1,40 +1,41 @@
-const priceCalc = () => {
-  const buyProduct = document.querySelector(".buy-product");
-  const sellProduct = document.querySelector(".sell-product");
+const buyProduct = document.querySelector(".buy-product");
+const sellProduct = document.querySelector(".sell-product");
+const price = document.getElementById("product-price")
 
+function priceCalc() {
   if (buyProduct || sellProduct) {
     const select_condition = document.getElementById("select2-selected-condition-container")
     const price = document.getElementById("product-price")
     const savings = document.getElementById("savings")
-
-
-    function calculateTotalPrice() {
-      var condition = document.getElementById("selected-condition").value
-
-      const conds = document.getElementById('conditions')
-      const conditions = JSON.parse(conds.dataset["conditions"])
-
-      var prod = document.getElementById('product')
-      var prod_price = parseInt(JSON.parse(prod.dataset["product"]).price)
-
-      if (buyProduct) {
-        var buy_ratio = conditions.find(element => element.name === condition).buy_ratio
-        var new_price = Math.round(prod_price * buy_ratio, 2)
-        price.innerText = `${new_price} €`;
-        savings.innerText = `save ${ 100 - (buy_ratio * 100) }%`;
-
-      } else if (sellProduct) {
-        var sell_ratio = conditions.find(element => element.name === condition).sell_ratio
-        var new_price = Math.round(prod_price * sell_ratio, 2)
-        price.innerText = `${new_price} €`;
-        savings.innerText = ``;
-      }
-    }
 
     select_condition.addEventListener("DOMSubtreeModified", calculateTotalPrice)
 
     calculateTotalPrice();
   }
 }
+
+function calculateTotalPrice() {
+  var condition = document.getElementById("selected-condition").value
+
+  const conds = document.getElementById('conditions')
+  const conditions = JSON.parse(conds.dataset["conditions"])
+
+  var prod = document.getElementById('product')
+  var prod_price = parseInt(JSON.parse(prod.dataset["product"]).price)
+
+  if (buyProduct) {
+    var buy_ratio = conditions.find(element => element.name === condition).buy_ratio
+    var new_price = Math.round(prod_price * buy_ratio, 2)
+    price.innerText = `${new_price} €`;
+    savings.innerText = `save ${ 100 - (buy_ratio * 100) }%`;
+
+  } else if (sellProduct) {
+    var sell_ratio = conditions.find(element => element.name === condition).sell_ratio
+    var new_price = Math.round(prod_price * sell_ratio, 2)
+    price.innerText = `${new_price} €`;
+    savings.innerText = ``;
+  }
+}
+
 
 export { priceCalc }
