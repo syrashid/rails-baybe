90873d0b3bd0456655f9c30fb02f95d358289f12
Sy Rashid <sy.rashid.11@gmail.com>
2019-09-28 23:43:05 -0700

    FIX: Updated product model to reflect current application best, must be careful during next seeding round

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_many :stock_products, dependent: :destroy
-  has_many :reviews, dependent: :destroy
+  # has_many :reviews, dependent: :destroy  TO DO: IMPLEMENT REVIEWS DOWN LINE
 
-  validates :name, presence: true, allow_blank: false
-  validates :description, presence: true, allow_blank: false
-  # validates :brand, presence: true, allow_nil: true
-  # validates :price, presence: true, allow_nil: true
-  # validates :age_min, presence: true, allow_blank: false, numericality: { only_integer: true }
-  # validates :age_max, presence: true, allow_blank: false, numericality: { only_integer: true }
-  # validates :gender, presence: true, allow_nil: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :brand, presence: true
+  validates :price, presence: true
   mount_uploader :photo, PhotoUploader
 
   include PgSearch::Model
