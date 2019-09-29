1f96eed8a51e961f614ae0dbb1d478b67bbc692e
Sy Rashid <sy.rashid.11@gmail.com>
2019-09-28 22:37:31 -0700

    STRUCT: Removed old filtercategory.js as it's no longer useful

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,19 +24,6 @@ class ProductsController < ApplicationController
     @rel_products = Product.all.sample(4)
   end
 
-  # def filter_category
-  #   if params[:query].present?
-  #     @searchprods = Product.search_by_name_and_description(params[:query]).includes(:category)
-  #     @products = @searchprods.select { |prod| prod.category.description == params[:cat] }
-  #   else
-  #     @products = Category.find_by(description: params[:cat]).products
-  #   end
-  #   @cat = params[:cat]
-  #   respond_to do |format|
-  #     format.js { render :filtercategory }
-  #   end
-  # end
-
   def filter_condition
     if params[:query].present?
       @searchprods = Product.search_by_name_and_description(params[:query]).includes(:stock_products)
